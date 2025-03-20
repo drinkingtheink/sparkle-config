776d0a8b8784ad5e722498f57467bba33394db58
Jason M Harrison <jason.m.harrison@gmail.com>
2025-03-19 20:52:56 -0500

    Add files via upload

---
+++ src/AppStage.vue
@@ -0,0 +1,358 @@
+<template>
+  <div class="app-stage" :class="{ 'hide-controls': !showControls }">
+    <button class="controls-toggle" @click="toggleControls">{{ this.showControls ? 'Hide Controls' : 'Show Controls'  }}</button>
+    
+    <h2>Configure Your Sparkle</h2>
+
+    <div class="animation-duration-input input-container" >
+      <label for="animation-duration-input">Animation Duration:</label>
+      <input 
+        id="animation-duration-input"
+        type="range" 
+        min="0"
+        max="20"
+        step="0.25"
+        v-model="animationDuration"
+      />
+      <span class="value-display"> {{  animationDuration }}s</span>
+    </div>
+
+    <section class="sparkle-container">
+      <Sparkle 
+        :lrgStarDelay="lrgStarDelay"
+        :medStarDelay="medStarDelay"
+        :smlStarDelay="smlStarDelay"
+        :animationDuration="animationDuration"
+      />
+
+      <div class="med-star-input input-container" >
+        <label for="med-star-input-delay">Bottom Star Animation Delay:</label>
+        <input 
+          id="med-star-input-delay"
+          type="range" 
+          min="0"
+          max="20"
+          step="0.25"
+          v-model="medStarDelay"
+        />
+        <span class="value-display"> {{  medStarDelay }}s</span>
+      </div>
+
+      <div class="sml-star-input input-container" >
+        <label for="sml-star-input-delay">Top Star Animation Delay:</label>
+        <input 
+          id="sml-star-input-delay"
+          type="range" 
+          min="0"
+          max="20"
+          step="0.25"
+          v-model="smlStarDelay"
+        />
+        <span class="value-display"> {{  smlStarDelay }}s</span>
+      </div>
+
+      <div class="lrg-star-input input-container" >
+        <label for="lrg-star-input-delay">Large Star Animation Delay:</label>
+        <input 
+          id="lrg-star-input-delay"
+          type="range" 
+          min="0"
+          max="20"
+          step="0.25"
+          v-model="lrgStarDelay"
+        />
+        <span class="value-display"> {{ lrgStarDelay }}s</span>
+      </div>
+    </section>
+
+    <section class="controls" >
+      <div>
+        <h3>SVG Image Download</h3>
+        
+        <button @click="downloadImage" class="download-img">Download Sparkle Image</button>
+      </div>
+      <div>
+      <h3>CSS Output</h3>
+        <section class="code-output">
+          <code>
+            #lrg-star,
+            #med-star,
+            #sml-star {
+                animation-name: twinkle;
+                animation-duration: {{ animationDuration }}s;
+                animation-delay: 0s;
+                animation-iteration-count: infinite;
+                transform-origin: center;
+            }
+
+            #lrg-star {
+              animation-delay: {{ lrgStarDelay }}s;
+            }
+
+            #med-star {
+              animation-delay: {{ medStarDelay }}s;
+            }
+
+            #sml-star {
+              animation-delay: {{ smlStarDelay }}s;
+            }
+          </code>
+        </section>
+
+        <button 
+          @click="copyCSS" 
+          class="download-css"
+          :class="{ 'success': copied }"  
+        >{{ copyButtonText }}</button>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Sparkle from './Sparkle.vue'
+const initialCopyText = 'Copy CSS'
+
+export default {
+  name: 'AppStage',
+  data() {
+    return {
+        lrgStarDelay: 0,
+        medStarDelay: 0.25,
+        smlStarDelay: 0.5,
+        animationDuration: 4,
+        imagePath: require('@/assets/sparkle-img.svg'),
+        showControls: true,
+        copyButtonText: initialCopyText,
+    }
+  },
+  components: {
+    Sparkle
+  },
+  props: {
+    msg: String
+  },
+  methods: {
+    downloadImage() {
+      // Create an anchor element
+      const link = document.createElement('a')
+      
+      // Set the download attribute with desired filename
+      link.download = 'sparkle-img.svg'
+      
+      // Point the link to the image URL
+      link.href = this.imagePath
+      
+      // Append to the document temporarily
+      document.body.appendChild(link)
+      
+      // Trigger the download
+      link.click()
+      
+      // Clean up
+      document.body.removeChild(link)
+    },
+    copyCSS() {
+      // Find the code element
+      const codeElement = document.querySelector('.code-output code');
+      const keyframes = `
+@keyframes twinkle {
+  0% {
+      fill: var(--bluefill);
+  }
+  10% {
+      fill: gray;
+      transform: scale(0.95);
+  }
+  30% {
+      fill: gray;
+      transform: scale(1.05);
+  }
+  50% {
+      fill: var(--bluefill);
+      transform: scale(1);
+  }
+  100% {
+      fill: var(--bluefill);
+  }
+}
+
+      `
+      
+      if (!codeElement) {
+        console.error('No code element found with selector ".code-output code"');
+        return;
+      }
+      
+      // Get the text content
+      const codeText = keyframes + codeElement.textContent.trim();
+      console.log(`COPIED TO CLIPBOARD >>>>>`);
+      console.log(`${codeText}`);
+      // Use the Clipboard API to copy the text
+      navigator.clipboard.writeText(codeText)
+        .then(() => {
+          // Change button state to indicate success
+          this.copied = true;
+          this.copyButtonText = 'Copied!';
+          
+          // Reset button state after 2 seconds
+          setTimeout(() => {
+            this.copied = false;
+            this.copyButtonText = initialCopyText;
+          }, 2000);
+        })
+        .catch(err => {
+          console.error('Failed to copy text: ', err);
+          this.copyButtonText = 'Failed to copy';
+          
+          setTimeout(() => {
+            this.copyButtonText = initialCopyText;
+          }, 2000);
+        });
+    },
+    toggleControls() {
+      this.showControls = !this.showControls;
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+:root {
+    --blue: #17a5ff;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.app-stage {
+  position: relative;
+  margin-top: 4rem;
+}
+
+.app-stage * {
+  transition: all 0.2s;
+}
+
+.sparkle-container {
+  margin: 0 auto;
+  width: 500px;
+  position: relative;
+  background-color: white;
+  padding: 1rem;
+}
+
+label,
+.value-display {
+  display: block;
+}
+
+.animation-duration-input {
+  width: 200px;
+  margin: 0 auto;
+  margin-bottom: 1rem;
+}
+
+.sml-star-input {
+  position: absolute;
+  right: -220px;
+  top: 0;
+}
+
+.lrg-star-input {
+  position: absolute;
+  left: -200px;
+  top: 20px;
+}
+
+.med-star-input {
+  position: absolute;
+  right: -200px;
+  bottom: 20px;
+}
+
+.code-output {
+  width: 400px;
+  margin: 0 auto 1rem auto;
+  border: 2px solid rgba(0,0,0,0.5);
+  padding: 10px;
+  border-radius: 5px;
+  background-color: white;
+  text-align: left;
+}
+
+.input-container {
+  background-color: rgba(0,0,0,0.8);
+  color: white;
+  padding: 10px 20px;
+  border-radius: 10px;
+}
+
+button {
+  color: white;
+  padding: 10px 15px;
+  background-color: var(--blue);
+  text-transform: uppercase;
+  border-radius: 10px;
+  transition: all 0.3s;
+  font-size: 1rem;
+}
+
+button:hover {
+  background-color: white;
+  color: var(--blue);
+  cursor: pointer;
+}
+
+.controls-toggle {
+  font-size: 0.8rem;
+  padding: 10px 20px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: -3rem;
+  margin: auto;
+  width: 200px;
+}
+
+.controls {
+  display: flex;
+  justify-content: center;
+}
+
+.controls div {
+  margin-right: 1rem;
+}
+
+.hide-controls .input-container,
+.hide-controls .controls {
+  opacity: 0;
+  transform: translateY(-10px);
+  pointer-events: none;
+}
+
+.download-css {
+  width: 150px;
+}
+
+.download-css.success {
+  background-color: rgb(7, 165, 7);
+}
+
+.download-css.success:hover {
+  color: white;
+}
+</style>
---
+++ src/Sparkle.vue
@@ -0,0 +1,81 @@
+<template>
+    <svg id="sparkle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 201.67 193.55">
+        <path
+            :style="{ 'animation-delay': lrgStarDelay + 's', 'animation-duration': animationDuration + 's' }"
+            id="lrg-star"
+            class="cls-1"
+            d="M99.63,68.77l17.31,6.4c2.61.97,2.61,4.66,0,5.63l-15.97,5.91c-14.88,5.51-25.85,18.32-28.98,33.88l-9.59,47.57c-.65,3.21-5.23,3.21-5.88,0l-9.59-47.57c-3.14-15.55-14.1-28.37-28.98-33.88l-15.97-5.91c-2.61-.97-2.61-4.66,0-5.63l17.31-6.4c14.15-5.23,24.8-17.1,28.49-31.73L56.54,2.27c.76-3.02,5.05-3.02,5.82,0l8.78,34.78c3.69,14.62,14.35,26.49,28.49,31.73Z"
+        />
+        <path
+            :style="{ 'animation-delay': smlStarDelay + 's', 'animation-duration': animationDuration + 's' }"
+            id="sml-star"
+            class="cls-1"
+            d="M194.34,43.53l6.59,2.44c.99.37.99,1.77,0,2.14l-6.08,2.25c-5.66,2.1-9.84,6.97-11.03,12.89l-3.65,18.1c-.25,1.22-1.99,1.22-2.24,0l-3.65-18.1c-1.19-5.92-5.37-10.8-11.03-12.89l-6.08-2.25c-.99-.37-.99-1.77,0-2.14l6.59-2.44c5.38-1.99,9.44-6.51,10.84-12.07l3.34-13.23c.29-1.15,1.92-1.15,2.21,0l3.34,13.23c1.4,5.57,5.46,10.08,10.84,12.07Z"
+        />
+        <path
+            :style="{ 'animation-delay': medStarDelay + 's', 'animation-duration': animationDuration + 's' }"
+            id="med-star"
+            class="cls-1"
+            d="M178.33,137.39l9.55,3.53c1.44.53,1.44,2.57,0,3.1l-8.81,3.26c-8.21,3.04-14.26,10.11-15.99,18.69l-5.29,26.24c-.36,1.77-2.89,1.77-3.24,0l-5.29-26.24c-1.73-8.58-7.78-15.65-15.99-18.69l-8.81-3.26c-1.44-.53-1.44-2.57,0-3.1l9.55-3.53c7.8-2.89,13.68-9.43,15.72-17.5l4.84-19.18c.42-1.67,2.79-1.67,3.21,0l4.84,19.18c2.04,8.07,7.92,14.61,15.72,17.5Z"
+        />
+    </svg>
+</template>
+
+<script>
+export default {
+  name: 'SparkleImg',
+  props: {
+    smlStarDelay: Number,
+    medStarDelay: Number,
+    lrgStarDelay: Number,
+    animationDuration: Number,
+  }
+}
+</script>
+
+<style>
+:root {
+    --bluefill: #17a5ff;
+}
+
+.cls-1 {
+    fill: var(--bluefill);
+    stroke-width: 0px;
+    transition: all 1s;
+    fill: initial;
+}
+
+#sparkle {
+    overflow: visible;
+}
+
+#lrg-star,
+#med-star,
+#sml-star {
+    animation-name: twinkle;
+    animation-delay: 0s;
+    animation-iteration-count: infinite;
+    transform-origin: center;
+}
+
+@keyframes twinkle {
+    0% {
+        fill: var(--bluefill);
+    }
+    10% {
+        fill: gray;
+        transform: scale(0.95);
+    }
+    30% {
+        fill: gray;
+        transform: scale(1.05);
+    }
+    50% {
+        fill: var(--bluefill);
+        transform: scale(1);
+    }
+    100% {
+        fill: var(--bluefill);
+    }
+}
+</style>
